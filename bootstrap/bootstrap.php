63e941d5795bdfa687f8539071172e98c634018d
Levente Löki <leventeloki@gmail.com>
2018-08-01 21:18:48 +0200

    Fixed bootstrap.php

--- bootstrap/bootstrap.php
+++ bootstrap/bootstrap.php
@@ -3,7 +3,7 @@
 require_once __DIR__ . '/../vendor/autoload.php';
 require_once __DIR__ . '/../vendor/lencse/rectum/src/functions.php';
 
-function createBootstrap(): \Lencse\Rectum\Framework\Application\Bootstrap {
+function createBootstrap(): \Lencse\Rectum\Framework\Application\ApplicationSetup {
     $config = require 'configuration.php';
-    return new \Lencse\Rectum\Framework\Application\Bootstrap($config);
+    return new \Lencse\Rectum\Framework\Application\ApplicationSetup($config);
 }
